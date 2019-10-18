7e8108615afae5a5e0816920b9681763d75ac386
Ubuntu <ubuntu@ip-172-31-29-218.us-east-2.compute.internal>
2019-10-18 14:04:36 +0000

    Add docstring to Week2/Assessment/04.py

--- Week 2/Assessment/04.py
+++ Week 2/Assessment/04.py
@@ -1,8 +1,18 @@
 """Write a function called "increasing(data)" that prints True if the
 input array is increasing or False otherwise. Hint: np.diff() returns
-the difference between consecutive elements of a sequence."""
+the difference between consecutive elements of a sequence.
+"""
 import numpy as np
 
 
 def increasing(data):
+    """Take an array and return whether the array is strictly increasing or not.
+    
+    Parameters:
+    ------
+    data: numpy array to be tested
+    Returns:
+    ------
+    Bool whether the array is strictly increasing or not.
+    """
     return (np.diff(data) > 0).all() == True
